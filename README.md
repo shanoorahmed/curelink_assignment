f1086597db658662b76443e154eefb6abc248770
shanoorahmed <shanoorahmed575@gmail.com>
2022-05-06 08:50:08 +0530

    code for creating and inserting data into database

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # curelink_assignment
+Python application which sends email to a list of subscribers at a specified time.
+
+# Steps to set up and run the project on your local machine:
+1) Make a new folder.
+2) Initialize an empty git repository inside the folder by running the command "git init" in your terminal.
+3) Clone the repository using "git clone https://github.com/shanoorahmed/curelink_assignment.git".
+4) Move into the folder, set up a new virtual enviroment and activate it.
+    4.1) For making a new virtual enviroment, paste "python3 -m venv venv" and run it.
+    4.2) For activating it:
+        Windows:
+        i) Set-ExecutionPolicy Unrestricted -scope process
+        ii) .\venv\Scripts\activate
+        Mac OS/Linux:
+        i) source venv/bin/activate
+5) Install all the dependencies using "pip install -r requirements.txt".
+6) To set up the database, paste "python create_db.py" and run it. The database with two tables, i.e. User and Newsletter, is created.
+7) Enter your email and password in the main.py file line 10 and 11.
+8) Run "python main.py".
+
+# Project Description
+
+
+# Future Improvements
+
+
+# References:
--- create_db.py
+++ create_db.py
@@ -1,26 +1,57 @@
 import sqlite3
 
-try:
-    sqliteConnection = sqlite3.connect('data.db')
-    sqlite_create_table_query_1 = '''CREATE TABLE User (
-                                id INTEGER PRIMARY KEY,
-                                name TEXT NOT NULL,
-                                email text NOT NULL UNIQUE);'''
-    sqlite_create_table_query_2 = '''CREATE TABLE Newsletter (
-                                id INTEGER PRIMARY KEY,
-                                topic TEXT NOT NULL,
-                                message text NOT NULL);'''
-    cursor = sqliteConnection.cursor()
-    print("Successfully Connected to SQLite")
-    cursor.execute(sqlite_create_table_query_1)
-    cursor.execute(sqlite_create_table_query_2)
-    sqliteConnection.commit()
-    print("SQLite table created")
-    cursor.close()
+def create_table():
+    try:
+        sqliteConnection = sqlite3.connect('data.db')
+        sqlite_create_table_query_1 = '''CREATE TABLE User (
+                                    id INTEGER PRIMARY KEY,
+                                    name TEXT NOT NULL,
+                                    email text NOT NULL UNIQUE);'''
+        sqlite_create_table_query_2 = '''CREATE TABLE Newsletter (
+                                    id INTEGER PRIMARY KEY,
+                                    topic TEXT NOT NULL,
+                                    message text NOT NULL);'''
+        cursor = sqliteConnection.cursor()
+        print("Successfully Connected to SQLite")
+        cursor.execute(sqlite_create_table_query_1)
+        cursor.execute(sqlite_create_table_query_2)
+        sqliteConnection.commit()
+        print("SQLite table created")
+        cursor.close()
 
-except sqlite3.Error as error:
-    print("Error while connecting to sqlite", error)
-finally:
-    if sqliteConnection:
-        sqliteConnection.close()
-        print("The SQLite connection is closed")
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
+
+def insert_data():
+    try:
+        sqliteConnection = sqlite3.connect('data.db')
+        cursor = sqliteConnection.cursor()
+        print("Successfully Connected to SQLite")
+
+        sqlite_insert_query_1 = """INSERT INTO User
+                            (id, name, email) 
+                            VALUES 
+                            (1,'','')"""
+        sqlite_insert_query_2 = """INSERT INTO Newsletter
+                            (id, topic, message) 
+                            VALUES 
+                            (1,'','')"""
+
+        cursor.execute(sqlite_insert_query_1)
+        cursor.execute(sqlite_insert_query_2)
+        sqliteConnection.commit()
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Failed to insert data into sqlite table", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
+
+create_table()
+insert_data()
